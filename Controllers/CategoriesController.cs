196f2bedee57dd317b79c1dda3159b5655eece46
Mlokiko <mlokos225@gmail.com>
2025-01-13 19:53:33 +0100

    Dodawanie kategori oraz ich wyświetlanie działa, usunąłem też niepotrzebne ograniczenia w DBcontext

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,6 +1,9 @@
 ﻿using ITstudyv4.Data;
-using ITstudyv4.ViewModels;
+using ITstudyv4.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace ITstudyv4.Controllers
 {
@@ -11,19 +14,37 @@ namespace ITstudyv4.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult ShowAllCategories()
         {
             var categories = _context.Categories.ToList();
             return View(categories);
         }
+
         public IActionResult AddNewCategory()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddNewCategory([Bind("Name,Description")] Categories category)
+        {
+            if (ModelState.IsValid)
+            {
+                Categories cat = new();
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ShowAllCategories));
+            }
+            return View(category);
+        }
+
         public IActionResult EditCategory()
         {
             return View();
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,10 +7,7 @@ namespace ITstudyv4.Data
 {
     public class AppDbContext : IdentityDbContext<ForumUser>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Categories> Categories { get; set; }
         public DbSet<Posts> Posts { get; set; }
         public DbSet<Ranks> Ranks { get; set; }
@@ -20,24 +17,38 @@ namespace ITstudyv4.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<ForumUser>()
-            .HasOne(s => s.Rank)
-            .WithMany()
-            .HasForeignKey(s => s.RankId);
+                .HasOne(s => s.Rank)
+                .WithMany()
+                .HasForeignKey(s => s.RankId);
             //.OnDelete(DeleteBehavior.Cascade);
 
-            builder.Entity<Posts>()
-            .HasOne(p => p.User)
-            .WithMany()
-            .HasForeignKey(p => p.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+            // Chyba niepotrzebne - IdentityUser ma już UserName i Email i powienien je sam sprawdzać
+            //builder.Entity<ForumUser>()
+            //    .Property(c => c.UserName)
+            //    .IsRequired();
 
-            builder.Entity<Posts>()
-            .HasOne(p => p.Thread)
-            .WithMany()
-            .HasForeignKey(p => p.ThreadId)
-            .OnDelete(DeleteBehavior.Cascade);
+            //builder.Entity<ForumUser>()
+            //    .Property(c => c.PasswordHash)
+            //    .IsRequired();
+
+            // Jak wymusić żeby było unique?
+            builder.Entity<ForumUser>()
+                .Property(c => c.Email)
+                .IsRequired();
+
+            builder.Entity<ForumUser>()
+                .Property(c => c.JoinDate)
+                .IsRequired()
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            // UserId i ThreadID z Posts nie daje tutaj jako required, sprawdzmy czy bez tego działa
+
+            builder.Entity<Ranks>()
+                .Property(c => c.Name)
+                .IsRequired();
 
             // Opis categori wymagany i nie większy od 255, Name wymagane
+            // ID ponoć będzie się inkrementować samo
             builder.Entity<Categories>()
                 .Property(c => c.Description)
                 .IsRequired()
@@ -46,6 +57,18 @@ namespace ITstudyv4.Data
             builder.Entity<Categories>()
                 .Property(c => c.Name)
                 .IsRequired();
+
+            builder.Entity<Posts>()
+                .HasOne(p => p.User)
+                .WithMany()
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Posts>()
+                .HasOne(p => p.Thread)
+                .WithMany()
+                .HasForeignKey(p => p.ThreadId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             // Może zadziałal i ustawi datę prawidłowo, trzeba potem to sprawdzić
             builder.Entity<Posts>()
@@ -65,12 +88,6 @@ namespace ITstudyv4.Data
                 .IsRequired()
                 .HasDefaultValue(false);
 
-            // UserId i ThreadID z Posts nie daje tutaj jako required, sprawdzmy czy bez tego działa
-
-            builder.Entity<Ranks>()
-                .Property(c => c.Name)
-                .IsRequired();
-
             builder.Entity<Threads>()
                 .Property(c => c.Title)
                 .IsRequired();
@@ -79,29 +96,6 @@ namespace ITstudyv4.Data
                 .Property(c => c.CreatedAt)
                 .IsRequired()
                 .HasDefaultValueSql("CURRENT_TIMESTAMP");
-
-            builder.Entity<ForumUser>()
-                .Property(c => c.UserName)
-                .IsRequired();
-
-            builder.Entity<ForumUser>()
-                .Property(c => c.PasswordHash)
-                .IsRequired();
-
-            // Jak wymusić żeby było unique?
-            builder.Entity<ForumUser>()
-                .Property(c => c.Email)
-                .IsRequired();
-
-            builder.Entity<ForumUser>()
-                .Property(c => c.JoinDate)
-                .IsRequired()
-                .HasDefaultValueSql("CURRENT_TIMESTAMP");
-
-            // Raczej rozwiązane przez relacje
-            //builder.Entity<SiteForumUser>()
-            //    .Property(c => c.Rank)
-            //    .IsRequired();
 
             // Czy to jest potrzebne? chat napisał to w momencie jak probowałem relacje naprawić
             base.OnModelCreating(builder);
