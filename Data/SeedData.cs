f32a8720aa6ef4fb569a70348f3162acb7934421
Mlokiko <mlokos225@gmail.com>
2025-01-14 14:34:14 +0100

    Seedowanie danych - użytkowników oraz ról

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -24,7 +24,7 @@ namespace ITstudyv4.Data
             // Tworzenie konta admina
             var adminEmail = "admin@example.com";
             var adminPassword = "Admin@123";
-            var adminUserName = "admin";
+            var adminUserName = "Admin";
 
             if (await userManager.FindByEmailAsync(adminEmail) == null)
             {
@@ -39,6 +39,48 @@ namespace ITstudyv4.Data
                 if (result.Succeeded)
                 {
                     await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+
+            // Tworzenie konta moderatora
+            var ModeratorEmail = "moderator@example.com";
+            var ModeratorPassword = "Moderator@123";
+            var ModeratorUserName = "Moderator";
+
+            if (await userManager.FindByEmailAsync(ModeratorEmail) == null)
+            {
+                var Moderator = new ForumUser
+                {
+                    UserName = ModeratorUserName,
+                    Email = ModeratorEmail,
+                    EmailConfirmed = true, // nie bawimy się w potwierdzanie maila, ale może zostać
+                };
+
+                var result = await userManager.CreateAsync(Moderator, ModeratorPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(Moderator, "Moderator");
+                }
+            }
+
+            // Tworzenie konta użytkownika
+            var uzytkownikEmail = "uzytkownik@example.com";
+            var uzytkownikPassword = "uzytkownik@123";
+            var uzytkownikUserName = "Uzytkownik";
+
+            if (await userManager.FindByEmailAsync(uzytkownikEmail) == null)
+            {
+                var uzytkownik = new ForumUser
+                {
+                    UserName = uzytkownikUserName,
+                    Email = uzytkownikEmail,
+                    EmailConfirmed = true, // nie bawimy się w potwierdzanie maila, ale może zostać
+                };
+
+                var result = await userManager.CreateAsync(uzytkownik, uzytkownikPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(uzytkownik, "Użytkownik");
                 }
             }
         }
