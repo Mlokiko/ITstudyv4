1b4b4eb2a58b64b60d53b94884e0b5aa5d0fd9ba
Mlokiko <mlokos225@gmail.com>
2025-01-16 16:56:26 +0100

    Próbuje zrobić wątki/tematy... jeszcze nie wyszły, ale przed wiekszymi zmianami chce zrobić "kopie"

--- Controllers/ThreadsController.cs
+++ Controllers/ThreadsController.cs
@@ -1,20 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITstudyv4.Data;
+using ITstudyv4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITstudyv4.Controllers
 {
     public class ThreadsController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public ThreadsController(AppDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        public IActionResult ShowAllThreads()
+        {
+            var threads = _context.Threads.ToList();
+            return View(threads);
+        }
+
         public IActionResult EditThread()
         {
+            
             return View();
         }
+
         public IActionResult AddNewThread()
         {
+            ViewBag.AllCategories = _context.Categories.Select(r => r.Name).ToList();
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddNewThread([Bind("Name,Categories")] Thread thread)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(thread);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ManageThreads));
+            }
+            return View(thread);
+        }
+
+        public IActionResult ManageThreads()
+        {
+            var threads = _context.Threads.ToList();
+            return View(threads);
         }
     }
 }
--- Models/Threads.cs
+++ Models/Threads.cs
@@ -1,10 +1,15 @@
-﻿namespace ITstudyv4.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITstudyv4.Models
 {
     public class Threads
     {
         public int Id { get; set; }
+        [Display(Name = "Tytuł")]
         public string? Title { get; set; }  // Not Null, jak to dodać?
+        [Display(Name = "Utworzono")]
         public DateTime CreatedAt { get; set; } // Dodać default aktualny czas 
+        [Display(Name = "Wyświetlenia")]
         public int Views { get; set; }
         public string? UserId { get; set; }
         public int CategoryId { get; set; }
@@ -13,6 +18,7 @@
         // W .sql mamy zapisane żeby się kaskadowo usuwało, będzie to trzeba sprawdzić czy prawidłowo się usuwa, nie robiliśmy tego na zajęciach
 
         public virtual ForumUser? User { get; set; }
+        [Display(Name = "Kategoria")]
         public virtual Categories? Category { get; set; }
         // Answers nie lepiej zamienić na posts? tzn. sama nazwe zminić, żeby się potem nie myliło
         public virtual Posts? Answers { get; set; }
